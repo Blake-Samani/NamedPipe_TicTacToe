71efefed1cdee9f224f3aacd4aa8a3ff885a8f55
Hong Sung <hs@cs.uco.edu>
2020-09-10 16:28:40 -0500

    update

--- prog1.cpp
+++ prog1.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-using namespace std;
-
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	const char* myfifo = "tmp/myfifo";
-	const char* myfifo2 = "tmp/myfifo2";
+int main(int argc, char **argv) {
+	// myfifo: prog1 => prog2
+	// myfifo2: prog2 => prog1
+	const char* myfifo = ".myfifo";
+	const char* myfifo2 = ".myfifo2";
 
 	// create the named pipe (FIFO)
 	mkfifo(myfifo, 0666);
 	mkfifo(myfifo2, 0666);
 
-	const int MAX = 256;
-	char rd_data[MAX];
-	string wr_string;
+	const int MAX = 1024;
+	char rd_data[MAX], wr_data[MAX];
+
+	printf("%s: READY!\n", argv[0]);
 
 	// prog1: write first
 	int fd = open(myfifo, O_WRONLY);
 	int	fd2 = open(myfifo2, O_RDONLY);
 
 	while (true) {
-		cout << "Enter a message: ";
-		getline(cin, wr_string);
+		printf("Enter a message: ");
+		fgets(wr_data, MAX, stdin);
 
-		const char* wr_data = wr_string.c_str();
 		write(fd, wr_data, strlen(wr_data) + 1);
 
 		read(fd2, rd_data, sizeof(rd_data));
-		string rd_string(rd_data);
-		cout << "prog1 received: " << rd_string << endl;
+		printf("prog1 received: %s", rd_data);
 	}
 }
--- prog2.cpp
+++ prog2.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
-
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char** argv)
 {
-	const char *myfifo = "tmp/myfifo";
-	const char* myfifo2 = "tmp/myfifo2";
+	const char *myfifo = ".myfifo";
+	const char* myfifo2 = ".myfifo2";
 
 	// create the named pipe (FIFO)
 	mkfifo(myfifo, 0666);
 	mkfifo(myfifo2, 0666);
 
-	const int MAX = 256;
-	char rd_data[MAX];
-	string wr_string;
+	const int MAX = 1024;
+	char rd_data[MAX], wr_data[MAX];
+
+	printf("%s: READY!\n", argv[0]);
 
 	// prog2: read first
 	int fd = open(myfifo, O_RDONLY);
@@ -27,13 +27,10 @@ int main()
 	while (true)
 	{
 		read(fd, rd_data, sizeof(rd_data));
-		string rd_string(rd_data);
-		cout << "prog2 received: " << rd_string << endl;
+		printf("prog1 received: %s", rd_data);
 
-		cout << "Enter a message: ";
-		getline(cin, wr_string);
-
-		const char* wr_data = wr_string.c_str();
+		printf("Enter a message: ");
+		fgets(wr_data, MAX, stdin);
 		write(fd2, wr_data, strlen(wr_data) + 1);
 	}
 }
